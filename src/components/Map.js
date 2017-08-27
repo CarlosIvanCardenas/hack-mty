4b2b074a98978fd21dabf81bf9759c7606c8b826
CarlosIvanCarenas <a00820062@itesm.mx>
2017-08-27 04:11:57 -0500

    fix

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,7 +8,6 @@ import {
     TouchableHighlight,
     ScrollView
 } from 'react-native'
-import MapView from 'react-native-maps';
 import commonStyles from "../styles/common.css";
 
 class Map extends Component{
@@ -16,7 +15,6 @@ class Map extends Component{
         super(props)
         this.state = {
             navTitle: '',
-
         }
     }
 
@@ -24,25 +22,13 @@ class Map extends Component{
         return(
             <View style={commonStyles.container} >
                 <Text style={commonStyles.title} >Beacon location</Text>
-                <MapView
-                    style={styles.map}
-                    
-                />
+
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  map: {
-    
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    width: 300, 
-    height: 450,
-  },
 });
 
 module.exports = Map;
